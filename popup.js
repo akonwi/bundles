5f99227f0e1a99a6b170457418dee44f4fe03bfe
akonwi <akonwi@gmail.com>
2015-05-06 00:55:43 -0500

    Add bundles, delete bundles. Add links to bundles

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+var cx = React.addons.classSet
+
 var Core = {
   actions: {},
   on: function(event, fn, ctx) {
@@ -17,21 +19,27 @@ var Core = {
   }
 }
 
-Core.on('replace-new-bundle-input', function() {
-  document.querySelector('.logo').textContent = 'Bundles'
-})
-Core.on('show-new-bundle-input', function() {
-  React.render(React.createElement(NewInput, null), document.querySelector('.logo'))
-})
-
 var Navbar = React.createClass({displayName: "Navbar",
   render: function() {
     return (
       React.createElement("div", null, 
-        React.createElement("h2", {className: "logo"}, "Bundles"), 
-        React.createElement(AddBtn, null)
+        React.createElement("div", {className: "nav-block small left"}, React.createElement(AddToBtn, null)), 
+        React.createElement("div", {className: "nav-block big"}, 
+          React.createElement(NewInput, {hidden: true}), 
+          React.createElement("h2", {className: "logo"}, "Bundles")
+        ), 
+        React.createElement("div", {className: "nav-block small right"}, React.createElement(AddBtn, null))
       )
     )
+  }
+})
+
+var AddToBtn = React.createClass({displayName: "AddToBtn",
+  onClick: function(e) {
+    e.preventDefault()
+  },
+  render: function() {
+    return React.createElement("a", {className: "nav-btn", href: "#", onClick: this.onClick}, "Add to")
   }
 })
 
@@ -41,28 +49,49 @@ var AddBtn = React.createClass({displayName: "AddBtn",
     Core.trigger('show-new-bundle-input')
   },
   render: function() {
-    return React.createElement("a", {id: "add-btn", href: "#", onClick: this.onClick}, "New")
+    return React.createElement("a", {className: "nav-btn", href: "#", onClick: this.onClick}, "New")
   }
 })
 
 var NewInput = React.createClass({displayName: "NewInput",
+  getInitialState: function() {
+    return { hidden: this.props.hidden }
+  },
+  componentWillMount: function() {
+    var component = this
+    Core.on('show-new-bundle-input', function() {
+      document.querySelector('.logo').classList.add('hidden')
+      component.setState({ hidden: false })
+    })
+    Core.on('hide-new-bundle-input', function() {
+      document.querySelector('.logo').classList.remove('hidden')
+      component.setState({ hidden: true })
+    })
+  },
   onkeypress: function(e) {
     if (e.keyIdentifier === 'Enter') {
       console.log(e)
       var name = e.target.value
       if (name.trim().length > 0) {
         Core.trigger('add-bundle', name)
-        Core.trigger('replace-new-bundle-input')
+        Core.trigger('hide-new-bundle-input')
         e.target.remove()
       }
     }
   },
   componentDidMount: function() {
     this.getDOMNode().onkeypress = this.onkeypress
-    this.getDOMNode().focus()
+  },
+  componentDidUpdate: function() {
+    if (!this.state.hidden)
+      this.getDOMNode().focus()
   },
   render: function() {
-    return React.createElement("input", {id: "new-bundle-input", type: "text", placeholder: "Bundle name..."})
+    var classes = cx({
+      'new-bundle-input': true,
+      hidden: this.state.hidden
+    })
+    return React.createElement("input", {className: classes, type: "text", placeholder: "Bundle name..."})
   }
 })
 
@@ -82,11 +111,19 @@ var BundleList = React.createClass({displayName: "BundleList",
          })
       })
     })
+    Core.on('delete-bundle', function() {
+      ChromeStorage.all(function(err, data) {
+        if (err)
+          console.error('Could not retrieve all items')
+        else
+          comp.setState({ bundles: data })
+      })
+    })
   },
   render: function() {
     var bundles = this.state.bundles
     return (
-      React.createElement("ul", {id: "bundles", className: "accordion"}, 
+      React.createElement("ul", {className: "bundles"}, 
         
           Object.keys(bundles).map(function(name) {
             var b = { name: name, links: bundles[name] }
@@ -99,14 +136,47 @@ var BundleList = React.createClass({displayName: "BundleList",
 })
 
 var BundleItem = React.createClass({displayName: "BundleItem",
+  getInitialState: function () {
+    return { links: this.props.bundle.links }
+  },
   onClick: function(e) {
     e.preventDefault()
     this.refs.links.getDOMNode().classList.toggle('open')
   },
+  addLink: function(e) {
+    e.preventDefault()
+    var name = this.props.bundle.name
+    var comp = this
+    chrome.tabs.getSelected(null, function(tab) {
+      ChromeStorage.get(name, function(err, links) {
+        links.push(tab.url)
+        this.set(name, links, function(err) {
+          if (err)
+            console.error("Could not save new bundle")
+          else
+            comp.setState({ links: links })
+        })
+      })
+    })
+  },
+  delete: function (e) {
+    e.preventDefault()
+    var comp = this
+    ChromeStorage.remove(this.props.bundle.name, function(err) {
+      if (err)
+        console.error("Couldn't delete %s from storage", comp.props.bundle.name)
+      else
+        Core.trigger('delete-bundle')
+    })
+  },
   render: function() {
     return (
-      React.createElement("li", {className: "bundle", onClick: this.onClick}, 
-        React.createElement("h4", {onClick: this.onClick},  this.props.bundle.name), 
+      React.createElement("li", {className: "bundle"}, 
+        React.createElement("div", {className: "title-bar"}, 
+          React.createElement("h4", {onClick: this.onClick},  this.props.bundle.name), 
+          React.createElement("button", {className: "btn", onClick: this.addLink}, "+"), 
+          React.createElement("button", {className: "btn", onClick: this.delete}, "x")
+        ), 
         React.createElement("ul", {className: "links", ref: "links"}, 
           
             this.props.bundle.links.map(function(link) {
--- src/popup.jsx
+++ src/popup.jsx
@@ -1,3 +1,5 @@
+var cx = React.addons.classSet
+
 var Core = {
   actions: {},
   on: function(event, fn, ctx) {
@@ -17,21 +19,27 @@ var Core = {
   }
 }
 
-Core.on('replace-new-bundle-input', function() {
-  document.querySelector('.logo').textContent = 'Bundles'
-})
-Core.on('show-new-bundle-input', function() {
-  React.render(<NewInput />, document.querySelector('.logo'))
-})
-
 var Navbar = React.createClass({
   render: function() {
     return (
       <div>
-        <h2 className='logo'>Bundles</h2>
-        <AddBtn />
+        <div className='nav-block small left'><AddToBtn /></div>
+        <div className='nav-block big'>
+          <NewInput hidden={true} />
+          <h2 className='logo'>Bundles</h2>
+        </div>
+        <div className='nav-block small right'><AddBtn /></div>
       </div>
     )
+  }
+})
+
+var AddToBtn = React.createClass({
+  onClick: function(e) {
+    e.preventDefault()
+  },
+  render: function() {
+    return <a className='nav-btn' href='#' onClick={this.onClick}>Add to</a>
   }
 })
 
@@ -41,28 +49,49 @@ var AddBtn = React.createClass({
     Core.trigger('show-new-bundle-input')
   },
   render: function() {
-    return <a id='add-btn' href='#' onClick={this.onClick}>New</a>
+    return <a className='nav-btn' href='#' onClick={this.onClick}>New</a>
   }
 })
 
 var NewInput = React.createClass({
+  getInitialState: function() {
+    return { hidden: this.props.hidden }
+  },
+  componentWillMount: function() {
+    var component = this
+    Core.on('show-new-bundle-input', function() {
+      document.querySelector('.logo').classList.add('hidden')
+      component.setState({ hidden: false })
+    })
+    Core.on('hide-new-bundle-input', function() {
+      document.querySelector('.logo').classList.remove('hidden')
+      component.setState({ hidden: true })
+    })
+  },
   onkeypress: function(e) {
     if (e.keyIdentifier === 'Enter') {
       console.log(e)
       var name = e.target.value
       if (name.trim().length > 0) {
         Core.trigger('add-bundle', name)
-        Core.trigger('replace-new-bundle-input')
+        Core.trigger('hide-new-bundle-input')
         e.target.remove()
       }
     }
   },
   componentDidMount: function() {
     this.getDOMNode().onkeypress = this.onkeypress
-    this.getDOMNode().focus()
+  },
+  componentDidUpdate: function() {
+    if (!this.state.hidden)
+      this.getDOMNode().focus()
   },
   render: function() {
-    return <input id='new-bundle-input' type='text' placeholder='Bundle name...' />
+    var classes = cx({
+      'new-bundle-input': true,
+      hidden: this.state.hidden
+    })
+    return <input className={classes} type='text' placeholder='Bundle name...' />
   }
 })
 
@@ -82,11 +111,19 @@ var BundleList = React.createClass({
          })
       })
     })
+    Core.on('delete-bundle', function() {
+      ChromeStorage.all(function(err, data) {
+        if (err)
+          console.error('Could not retrieve all items')
+        else
+          comp.setState({ bundles: data })
+      })
+    })
   },
   render: function() {
     var bundles = this.state.bundles
     return (
-      <ul id='bundles' className='accordion'>
+      <ul className='bundles'>
         {
           Object.keys(bundles).map(function(name) {
             var b = { name: name, links: bundles[name] }
@@ -99,14 +136,47 @@ var BundleList = React.createClass({
 })
 
 var BundleItem = React.createClass({
+  getInitialState: function () {
+    return { links: this.props.bundle.links }
+  },
   onClick: function(e) {
     e.preventDefault()
     this.refs.links.getDOMNode().classList.toggle('open')
   },
+  addLink: function(e) {
+    e.preventDefault()
+    var name = this.props.bundle.name
+    var comp = this
+    chrome.tabs.getSelected(null, function(tab) {
+      ChromeStorage.get(name, function(err, links) {
+        links.push(tab.url)
+        this.set(name, links, function(err) {
+          if (err)
+            console.error("Could not save new bundle")
+          else
+            comp.setState({ links: links })
+        })
+      })
+    })
+  },
+  delete: function (e) {
+    e.preventDefault()
+    var comp = this
+    ChromeStorage.remove(this.props.bundle.name, function(err) {
+      if (err)
+        console.error("Couldn't delete %s from storage", comp.props.bundle.name)
+      else
+        Core.trigger('delete-bundle')
+    })
+  },
   render: function() {
     return (
-      <li className='bundle' onClick={this.onClick}>
-        <h4 onClick={this.onClick}>{ this.props.bundle.name }</h4>
+      <li className='bundle'>
+        <div className='title-bar'>
+          <h4 onClick={this.onClick}>{ this.props.bundle.name }</h4>
+          <button className='btn' onClick={this.addLink}>+</button>
+          <button className='btn' onClick={this.delete}>x</button>
+        </div>
         <ul className='links' ref='links'>
           {
             this.props.bundle.links.map(function(link) {
