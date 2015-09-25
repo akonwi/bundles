460454b28b22113a0d27b1f69bafe8201f7bbe44
akonwi <akonwi@gmail.com>
2015-09-24 22:31:26 -0500

    Refactor in logo-input.jsx

--- src/components/logo-input.jsx
+++ src/components/logo-input.jsx
@@ -1,4 +1,4 @@
-import NewBundleInput from './new-bundle-input.jsx'
+import NewBundleInputFactory from './new-bundle-input.jsx'
 
 export default function(Core) {
   return React.createClass({
@@ -14,9 +14,9 @@ export default function(Core) {
       })
     },
     render() {
-      let Input = NewBundleInput(Core)
+      let NewBundleInput = NewBundleInputFactory(Core)
       if (this.state.showInput)
-        return <Input />
+        return <NewBundleInput />
       else
         return <h2 className='logo'>Bundles</h2>
     }
