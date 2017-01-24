40337f332f62f710ad8b612b684ef5c9fff92554
akonwi <akonwi@gmail.com>
2017-01-24 13:36:49 -0600

    Update build script

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-mkdir -p ../bundles_release
+rm ../bundles_release.zip
+rm -rf ../bundles_release
+mkdir ../bundles_release
 cp -r ./assets ../bundles_release/
 cp -r ./dist ../bundles_release/
 cp -r ./lib ../bundles_release/
-cp -r ./manifest.json ../bundles_release/
-cp -r ./popup.html ../bundles_release/
-cp -r ./styles.css ../bundles_release/
+cp ./manifest.json ../bundles_release/
+cp ./popup.html ../bundles_release/
+cp ./styles.css ../bundles_release/
 zip -r ../bundles_release.zip ../bundles_release
